e3ab5d2857e017534ccd604cef28945b3ed33063
rajneesh-vinsol <rajneesh@vinsol.com>
2019-01-28 18:12:24 +0530

    fix serialize error

--- mystore/app/models/spree/user_activity_log.rb
+++ mystore/app/models/spree/user_activity_log.rb
@@ -2,7 +2,8 @@ module Spree
 
   class UserActivityLog < Spree::Base
 
-    serialize :flash, :errors
+    serialize :flash
+    serialize :entity_errors
 
     belongs_to :user, class_name: 'Spree::User'
     validates :user, :response_code, :ip_address, presence: true
