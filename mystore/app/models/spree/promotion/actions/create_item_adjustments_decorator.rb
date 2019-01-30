404b10a8cadaa205fa58e11210a1d5c2a3e6521b
rajneesh-vinsol <rajneesh@vinsol.com>
2019-01-30 12:15:53 +0530

    refactor create_line_item_adjustment code

--- mystore/app/models/spree/promotion/actions/create_item_adjustments_decorator.rb
+++ mystore/app/models/spree/promotion/actions/create_item_adjustments_decorator.rb
@@ -7,13 +7,25 @@ module Spree
 
         def compute_amount(line_item)
           return 0 unless promotion.line_item_actionable?(line_item.order, line_item)
-          if line_item.variant.on_sale? && get_preference(:compare_with_sale_price) == '0'
-            return 0
+          if line_item.variant.on_sale?
+            if !compare_with_sale_price?
+              return 0
+            elsif compare_with_sale_price? && sale_price_better_deal?(line_item)
+              return 0
+            else
+              [line_item.amount, compute(line_item)].min * -1
+            end
+          else
+            [line_item.amount, compute(line_item)].min * -1
           end
-          if get_preference(:compare_with_sale_price) == '1' && line_item.variant.price - compute(line_item) > line_item.amount
-            return 0
-          end
-          [line_item.amount, compute(line_item)].min * -1
+        end
+
+        private def compare_with_sale_price?
+          get_preference(:compare_with_sale_price) == '1'
+        end
+
+        private def sale_price_better_deal?(line_item)
+          line_item.variant.price - compute(line_item) > line_item.amount
         end
 
       end
